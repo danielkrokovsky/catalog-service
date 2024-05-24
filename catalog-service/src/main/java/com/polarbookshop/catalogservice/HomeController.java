74cc798226f404d017257de2a6fa173951c1018e
daniel krokovsky <danielkrokovsky@hotmail.com>
2024-05-24 11:16:13 -0300

    ajste

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 }
