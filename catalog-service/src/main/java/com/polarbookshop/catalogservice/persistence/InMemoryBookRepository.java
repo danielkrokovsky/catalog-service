71c3bbba4ee2dc947704d5c0b82821f0c3e2d8a0
daniel krokovsky <danielkrokovsky@hotmail.com>
2024-05-25 18:12:42 -0300

    ajuste

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
@@ -40,4 +40,64 @@ public class InMemoryBookRepository implements BookRepository {
 		books.remove(isbn);
 	}
 
+	@Override
+	public <S extends Book> Iterable<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Book> findById(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<Book> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Book entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends Long> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Book> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
